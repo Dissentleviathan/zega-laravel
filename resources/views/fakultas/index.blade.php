46e6c2c8058cb09fe1a54b2a8777516536cd83ff
Dissentleviathan <maghasp22@gmail.com>
2023-04-27 15:05:21 +0700

    1

--- resources/views/fakultas/index.blade.php
+++ resources/views/fakultas/index.blade.php
@@ -5,28 +5,33 @@
 
 
 @section('content')
-    @section('credit','Universitas Multi Data Palembang')
+@section('credit', 'Universitas Multi Data Palembang')
 
-    <table class="table table-hover">
-        <thead>
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>Nama Fakultas</th>
+            <TH>Nama Dekan</TH>
+            <th>Nama Wakil Dekan</th>
+            <th>Prodi</th>
+            <th>Created At</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($dataFakultas as $item)
             <tr>
-                <th>Nama Fakultas</th>
-                <TH>Nama Dekan</TH>
-                <th>Nama Wakil Dekan</th>
-                <th>Created At</th>
+                <td>{{ $item->nama_fakultas }}</td>
+                <td>{{ $item->nama_dekan }}</td>
+                <td>{{ $item->nama_wakil_dekan }}</td>
+                <td>
+                    @foreach ($item->prodi as $prodi)
+                        {{ $prodi->nama_prodi }}
+                    @endforeach
+                </td>
+                <td>{{ $item->created_at }}</td>
             </tr>
-        </thead>
-        <tbody>
-            @foreach ($dataFakultas as $item)
-            <tr>
-                <td>{{$item->nama_fakultas}}</td>
-                 <td>{{$item->nama_dekan}}</td>
-                 <td>{{$item->nama_wakil_dekan}}</td>
-                 <td>{{$item->created}}</td>
-            </tr>
-            @endforeach
-        </tbody>
+        @endforeach
+    </tbody>
 
-    </table>
+</table>
 @endsection
-
